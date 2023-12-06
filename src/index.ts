57c829decc67216c7257f05d133944956fddaa85
SoftwareX Plus <119097812+softwarexplus@users.noreply.github.com>
2023-12-06 10:28:15 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ export default function power(base: number, exponent: number): number {
     if (exponent < 0) {
         return 1 / base ** Math.abs(exponent)
     }
+
     if (exponent > 1000) {
         throw new Error("exponent is too large")
     }
